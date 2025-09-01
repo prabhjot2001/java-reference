591f20a56460ba763c1d49266467c93614b2f832
prabhjot2001 <lazycoder001@gmail.com>
2025-09-01 22:46:20 +0530

    multi dimensional array

--- 03_Arrays/MultiDimensionalArray.java
+++ 03_Arrays/MultiDimensionalArray.java
@@ -6,6 +6,7 @@ public class MultiDimensionalArray {
     public static void main(String[] args) {
         // 2D array
         Scanner sc = new Scanner(System.in);
+       
         int[][] twoDimensionalArray = new int[3][4];
         for (int i = 0; i < twoDimensionalArray.length; i++) {
             System.out.println("enter " + (i + 1) + "st " + (twoDimensionalArray[i].length) + " values");
@@ -19,6 +20,27 @@ public class MultiDimensionalArray {
                 System.out.print(n + " ");
             }
             System.out.println();
+        }
+        
+
+        // 3D array
+        int[][][] threeDimensionalArray = new int[2][3][4];
+        for(int x=0; x<threeDimensionalArray.length; x++){
+            for(int y=0; y<threeDimensionalArray[x].length; y++){
+                for(int z=0; z<threeDimensionalArray[x][y].length; z++){
+                    threeDimensionalArray[x][y][z] = sc.nextInt();
+                }
+            }
+        }
+
+        // printing 3D array
+        for(int[][] x : threeDimensionalArray){
+            for(int[] y : x){
+                for(int z : y){
+                    System.out.print(z + " ");
+                }
+                System.out.println();
+            }
         }
     }
 }
