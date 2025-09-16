a385be3a0a0421587d49d296c6edc9b00d5900a4
Prabhjot Singh <lazycoder001@gmail.com>
2025-09-17 00:37:14 +0530

    datatypes in java

---
+++ 00_Basics/DataTypes.java
@@ -0,0 +1,37 @@
+public class DataTypes{
+    public static void main(String[] args){
+
+        
+        //    1. Primitive Data Type: These are the basic building blocks that store simple values directly in memory.
+        //    Examples of primitive data types are:
+        //     boolean
+        //     char
+        //     byte
+        //     short
+        //     int
+        //     long
+        //     float
+        //     double 
+         
+        boolean b = true;
+        byte by = 127; //max value that you can store in byte is 127
+        short sh = 32767; // max value that you can store in short is 32767 i.e. 2^15-1
+        int i = 123123;
+        long l = 23423423l;
+        float fl = 234.2f;
+        double d = 143.234;
+
+
+        // Non-Primitive Data Types (Object Types): These are reference types that store memory addresses of objects. 
+        // Examples of Non-primitive data types are
+        // String
+        // Array
+        // Class
+        // Interface
+        // Object
+
+        String str = "This is simple string";
+        int[] arr = new int[5];
+        
+    }
+}
