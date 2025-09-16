aed4003ad62c9258f9095dace50ec4541435c616
Prabhjot Singh <lazycoder001@gmail.com>
2025-09-17 00:19:02 +0530

    comments in java

---
+++ 00_Basics/Comments.java
@@ -0,0 +1,13 @@
+public class Comments{
+    public static void main(String[] args){
+        // This is single line comment
+
+        /*
+          This is multiline comment
+        */
+
+       /**
+        * This is documentation style comment
+        */
+    }
+}
